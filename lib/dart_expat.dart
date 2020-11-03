ce6491179db3c26208346ef5b48d4f75c36e4f4c
J-P Nurmi <jpnurmi@gmail.com>
2020-11-03 21:12:14 +0100

    Init simple Dart package template

---
+++ lib/dart_expat.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_expat;
+
+export 'src/dart_expat_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
